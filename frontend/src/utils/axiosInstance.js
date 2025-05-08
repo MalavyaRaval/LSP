13c54127aadd0425f109aadc1f238079c90408be
MalavyaRavaL <malavyaraval@gmail.com>
2025-05-07 20:14:55 -0700

    changes for frontend deployment

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://lsp-backend.onrender.com',
-    withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL || "https://lsp-backend.onrender.com",
+  withCredentials: true,
 });
 
 export default axiosInstance;
